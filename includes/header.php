80cfb78f85e47d8eefaf048c8f2cf3f91754e79c
AlifRipaldi <alifripaldi05@gmail.com>
2026-05-11 23:32:32 +0800

    perubahan beranda

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Arena Sport | Booking System</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=7">
 </head>
 <body>
     <header>
